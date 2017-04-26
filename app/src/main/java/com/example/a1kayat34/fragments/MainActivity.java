feb3197c0307d9aa964e9d91d191d91d038f2ffa
Kayaya <1kayat34@solent.ac.uk>
2017-04-26 14:42:56 +0100

    Exercise 1 of fragments complete.

--- app/src/main/java/com/example/a1kayat34/fragments/MainActivity.java
+++ app/src/main/java/com/example/a1kayat34/fragments/MainActivity.java
@@ -8,12 +8,13 @@ import android.view.MenuItem;;import org.osmdroid.views.MapView;
 
 public class MainActivity extends Activity {
 
-    MapView map;
+    //MapView map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
     }
@@ -25,4 +26,22 @@ public class MainActivity extends Activity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragmentID);
+
+        if(item.getItemId() == R.id.london){
+            mapFragment.getNewLocation(51.51, -0.1);
+            return true;
+        }
+        if(item.getItemId() == R.id.paris){
+            mapFragment.getNewLocation(48.85, 2.34);
+            return true;
+        }
+        if(item.getItemId() == R.id.new_york){
+            mapFragment.getNewLocation(40.75, -74);
+            return true;
+        }
+        return false;
+    }
 }
--- app/src/main/java/com/example/a1kayat34/fragments/MapFragment.java
+++ app/src/main/java/com/example/a1kayat34/fragments/MapFragment.java
@@ -3,14 +3,22 @@ package com.example.a1kayat34.fragments;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
 /**
  * Created by 1kayat34 on 24/04/2017.
  */
 public class MapFragment extends Fragment {
+
+    MapView map;
+
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -20,4 +28,25 @@ public class MapFragment extends Fragment {
     {
         return inflater.inflate(R.layout.mapfrag, parent);
     }
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+        Activity activity = getActivity();
+
+        Configuration.getInstance().load(activity, PreferenceManager.getDefaultSharedPreferences(activity));
+
+        map = (MapView) getView().findViewById(R.id.map1);
+        map.setBuiltInZoomControls(true);
+        map.getController().setZoom(13);
+        map.getController().setCenter(new GeoPoint(40.1,22.5));
+
+        // Now you have an activity, you can create the map as before...
+    }
+
+    public void getNewLocation(double latitude, double longitude){
+        GeoPoint ll = new GeoPoint(latitude, longitude);
+        map.getController().setCenter(ll);
+    }
+
+
 }
